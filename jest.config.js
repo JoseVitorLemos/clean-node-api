f8205d2f4bfc8f43004b7b5d504b1d2f1e8b1ab6
José Vitor <jose81073175@gmail.com>
2022-04-14 00:11:42 -0300

    refactor: add ignore path to covegare

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ module.exports = {
 		"<rootDir>/src/presentation/protocols/index.ts",
 		"<rootDir>/src/presentation/controllers/signup/signup-protocols.ts",
 		"<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts",
-		"<rootDir>/src/main/server.ts"
+		"<rootDir>/src/main/server.ts",
+    "<rootDir>/src/main/config/env.ts"
 	],
 }
